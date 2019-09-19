87f24149ef29cb91fc273ef656a3cc42c6e40e8b
R4506 <R4506@lab.aca>
2019-09-19 17:53:23 -0500

    Added firebase

--- hosting/src/main.js
+++ hosting/src/main.js
@@ -1,5 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
+import * as firebase from 'firebase';
+import 'firebase/firestore'
+firebase.initializeApp({
+  apiKey: "AIzaSyCps14xHhcXnJ49vGL7oOTn6LZZMDZ1lQI",
+  authDomain: "foodex-backend.firebaseapp.com",
+  databaseURL: "https://foodex-backend.firebaseio.com",
+  projectId: "foodex-backend",
+  storageBucket: "foodex-backend.appspot.com",
+  messagingSenderId: "676398625241",
+  appId: "1:676398625241:web:d9a02a4bac557419e269c8"
+});
+
+import Vue from 'vue';
+import App from './App.vue';
 
 Vue.config.productionTip = false
 
