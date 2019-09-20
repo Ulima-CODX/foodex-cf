f1a3e0c125490ad96eb318a7ffcf95d1d81927d7
R4506 <R4506@lab.aca>
2019-09-19 19:53:01 -0500

    WIP registration

---
+++ hosting/src/components/Register.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="register">
+        <h1>{{ msg }}</h1>
+        
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Register',
+    props: {
+
+    }
+}
+</script>
