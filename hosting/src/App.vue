ea9f52bc0df077b21c338376574a73125dc5f738
Daniel R <20162614@ulima.edu.pe>
2019-09-20 18:58:37 -0500

    Tested Register

--- hosting/src/App.vue
+++ hosting/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <EmployeeRegister />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import EmployeeRegister from "./components/Employee/Register";
 
 export default {
   name: "app",
   components: {
-    HelloWorld
+    EmployeeRegister
   }
 };
 </script>
