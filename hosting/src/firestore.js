71c448ebe86c69e5fe5096ff22158f94d5871ab7
Daniel R <20162614@ulima.edu.pe>
2019-09-20 18:58:59 -0500

    Added firebase auth

--- hosting/src/firestore.js
+++ hosting/src/firestore.js
@@ -4,19 +4,21 @@ import { firestorePlugin } from "vuefire";
 Vue.use(firestorePlugin);
 
 import firebase from "firebase/app";
-import  "firebase/firestore";
+firebase.initializeApp({
+  apiKey: "AIzaSyCps14xHhcXnJ49vGL7oOTn6LZZMDZ1lQI",
+  authDomain: "foodex-backend.firebaseapp.com",
+  databaseURL: "https://foodex-backend.firebaseio.com",
+  projectId: "foodex-backend",
+  storageBucket: "foodex-backend.appspot.com",
+  messagingSenderId: "676398625241",
+  appId: "1:676398625241:web:d9a02a4bac557419e269c8"
+});
 
-export const db = firebase.initializeApp({
-    apiKey: "AIzaSyCps14xHhcXnJ49vGL7oOTn6LZZMDZ1lQI",
-    authDomain: "foodex-backend.firebaseapp.com",
-    databaseURL: "https://foodex-backend.firebaseio.com",
-    projectId: "foodex-backend",
-    storageBucket: "foodex-backend.appspot.com",
-    messagingSenderId: "676398625241",
-    appId: "1:676398625241:web:d9a02a4bac557419e269c8"
-}).firestore()
+import "firebase/auth";
+export const auth = firebase.auth();
 
-const { TimeStamp, GeoPoint } = firebase.firestore
-export { TimeStamp, GeoPoint }
+import "firebase/firestore";
+export const db = firebase.firestore();
 
-db.settings({ timestampsInSnapshots: true })
+const { TimeStamp, GeoPoint } = firebase.firestore;
+export { TimeStamp, GeoPoint };
