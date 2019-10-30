95dae3c25b248f9ff5318964f307987d2e72a8aa
Sergio721 <oigres721@gmail.com>
2019-10-30 17:44:49 -0500

    navbar

---
+++ hosting/src/views/components/Navbar.vue
@@ -0,0 +1,33 @@
+<template>
+    <nav>
+        <v-app-bar color="#E41E2B" flat>
+            <v-img
+            class="mx-2"
+            src="@/resources/assets/foodex1.png"
+            max-height="65"
+            max-width="65"
+            contain
+            alt="logo"
+            > 
+            </v-img>
+            <v-toolbar-title class="text-uppercase">
+                <span class="font-weight-black">Foodex</span>
+            </v-toolbar-title>
+            <v-spacer></v-spacer>
+
+            
+       </v-app-bar>
+
+    </nav>
+
+</template>
+
+
+
+<script>
+export default {
+    data(){
+
+    }
+}
+</script>
