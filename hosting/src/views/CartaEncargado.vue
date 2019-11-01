dbff9fe3ca01fff4e0e7087323c36a34a222f400
Sergio721 <oigres721@gmail.com>
2019-11-01 12:17:39 -0500

    Tabs

--- hosting/src/views/CartaEncargado.vue
+++ hosting/src/views/CartaEncargado.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-card max-width="800" class="mx-auto">
+  <v-card width="800" class="mx-auto mt-5 ">
     <v-toolbar color="#E41E2B" dark>
       <v-toolbar-title>Carta</v-toolbar-title>
 
@@ -9,9 +9,7 @@
         <v-icon>mdi-magnify</v-icon>
       </v-btn>
 
-      <v-btn icon>
-        <v-icon>mdi-checkbox-marked-circle</v-icon>
-      </v-btn>
+
     </v-toolbar>
 
     <v-list two-line>
