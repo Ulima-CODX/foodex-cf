eb5dc81801240b3cd26312dfc037141fc2a58567
Sergio721 <oigres721@gmail.com>
2019-11-13 20:17:08 -0500

    Crear Plato

---
+++ hosting/src/views/manager/MenuNew.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-card class="flex">
+    <!--Toolbar-->
+    <v-toolbar color="#E41E2B" dark flat>
+      <v-toolbar-title>Add New Dish</v-toolbar-title>
+    </v-toolbar>
+    <!--Establishment Form-->
+    <v-card-text>
+      <v-form>
+        <!--Name-->
+        <v-text-field single-line label="Name" v-model="form.name" />
+        <!--Description-->
+        <v-text-field
+          single-line
+          label="Description"
+          v-model="form.description"
+        />
+      </v-form>
+    </v-card-text>
+    <v-divider />
+    <!--Create Button-->
+    <v-card-actions>
+      <v-spacer />
+      <v-btn
+        color="info"
+        @click="
+          create(form);
+          dialog = false;
+        "
+        >Crear</v-btn
+      >
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+//Controller Import
+import { create } from "@/controllers/manager/menu";
+//Schema Import
+import { MenuCollection, MenuDocument } from "@/models/menu/schema";
+import { DishCollection, DishDocument } from "@/models/dish/schema";
+
+export default {
+    data(){
+        return{
+            create,
+            form: {
+                name:"",
+                description:""
+            }
+        }
+    }
+    
+}
+</script>
