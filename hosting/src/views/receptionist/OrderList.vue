ff885592590077849020652b0338ea631a78d260
Andre Cabrera <andrecv319@gmail.com>
2019-11-21 16:57:21 -0500

    order-reservation_list

--- hosting/src/views/receptionist/OrderList.vue
+++ hosting/src/views/receptionist/OrderList.vue
@@ -3,33 +3,41 @@
     <!--Toolbar-->
     <v-toolbar color="#E41E2B" dark flat>
       <v-toolbar-title>Orders</v-toolbar-title>
+      <v-spacer/>
+      <v-btn icon>
+        <v-icon>mdi-checkbox-marked-circle</v-icon>
+      </v-btn>
     </v-toolbar>
     <!--List-->
     <v-list two-line>
     <v-list-item-group>
       <v-list-item v-for="order in orders" :key="order.id">
         <v-list-item-content>
-          <v-list-item-title v-text="`ID: ${order.id}`"/>
+          <!--v-list-item-title v-text="`ID: ${order.id}`"/-->
+          <v-list-item-title v-text="`Juanito Alcachofa`"/>
+          <!--v-list-item-subtitle
+          class="text-primary"
+          v-text="`Client ID: ${order.data.client_id}`"/-->
           <v-list-item-subtitle
-          class="text--primary"
-          v-text="`Client ID: ${order.data.client_id}`"/>
-          <v-list-item-subtitle
-          class="text--primary"
+          class="text-primary"
           v-text="`Dish IDs: ${Object.entries(order.data.dish_ids)}`"/>
           <v-list-item-subtitle
-          class="text--primary"
+          class="text-primary"
           v-text="`Status: ${order.data.status}`"/>
           <v-list-item-subtitle
-          class="text--primary"
+          class="text-primary"
           v-text="`Comment: ${order.data.comment}`"/>
           <v-list-item-subtitle
-          class="text--primary"
+          class="text-primary"
           v-text="`Sub-total: ${order.data.total}`"/>
           <v-list-item-subtitle
-          class="text--primary"
+          class="text-primary"
+          v-text="`Discount: ${order.data.discount}`"/>
+          <v-list-item-subtitle
+          class="text-primary"
           v-text="`Total: ${order.data.total_to_pay}`"/>
           <v-list-item-subtitle
-          class="text--primary"
+          class="text-primary"
           v-text="`Time Stamp: ${order.data.timestamp}`"/>
         </v-list-item-content>
       <v-list-item-action>
